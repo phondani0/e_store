8bc519e3d6cede3ee16040ce34e8fb03b096227f
Ankit <ankitphondani52@gmail.com>
2020-05-22 18:57:52 +0530

    Fixed createUser component

--- admin/src/Users/UserList.js
+++ admin/src/Users/UserList.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { List, Datagrid, TextField, EmailField } from 'react-admin';
+import { List, Datagrid, TextField, EmailField, DateField } from 'react-admin';
 
-export const UserList = props => (
-  <List {...props}>
-    <Datagrid rowClick="edit">
-      <TextField source="id" />
-      <TextField source="first_name" />
-      <TextField source="last_name" />
-      <EmailField source="email" />
-      <TextField source="mobile" />
-      <TextField source="created_at" />
-      <TextField source="updated_at" />
-    </Datagrid>
-  </List>
-);
+function CustomDateField(props) {
+  const newRecord = {
+    [props.source]: parseInt(props.record[props.source])
+  }
+  return <DateField showTime {...props} record={newRecord} />
+}
+
+
+export const UserList = props => {
+  console.log(props)
+  return (
+    <List {...props}>
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <TextField source="first_name" />
+        <TextField source="last_name" />
+        <EmailField source="email" />
+        <TextField source="mobile" />
+        <CustomDateField source="created_at" />
+        <CustomDateField source="updated_at" />
+      </Datagrid>
+    </List >
+  );
+}
