f0edc2e2715e532df067252bbce0e341e306cb84
Ankit <ankitphondani52@gmail.com>
2020-06-11 08:49:10 +0530

    changed user id from int to string

--- admin/src/dataProvider.js
+++ admin/src/dataProvider.js
@@ -8,30 +8,26 @@ const myBuildQuery = introspection => (fetchType, resource, params) => {
   const builtQuery = buildQuery(introspection)(fetchType, resource, params);
   console.log(fetchType, resource, params);
 
-  if (resource === 'User' && fetchType === 'GET_ONE') {
-    console.log(fetchType, resource, params);
-    // let _id = +params.id;
-    return {
-      // Use the default query variables and parseResponse
-      ...builtQuery,
-      // Override the query
-      query: gql `
-                query User($id: Int!) {
-                  data: User(id: $id) {
-                      id
-                      first_name
-                      last_name
-                      email
-                      mobile
-                      created_at
-                      updated_at    
-                    }              
-                }`,
-      variables: {
-        id: parseInt(params.id)
-      }
-    };
-  }
+  // if (resource === 'User' && fetchType === 'GET_ONE') {
+  //   console.log(fetchType, resource, params);
+  //   return {
+  //     // Use the default query variables and parseResponse
+  //     ...builtQuery,
+  //     // Override the query
+  //     query: gql `
+  //               query User($id: String!) {
+  //                 data: User(id: $id) {
+  //                     id
+  //                     first_name
+  //                     last_name
+  //                     email
+  //                     mobile
+  //                     created_at
+  //                     updated_at    
+  //                   }              
+  //               }`
+  //   };
+  // }
 
   return builtQuery;
 };
