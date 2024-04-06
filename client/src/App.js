c7d398d98187c7f753676528ae6ee622b2707f24
phondani0 <ankitphondani52@gmail.com>
2024-04-07 04:13:41 +0530

    feat: implemented auth check for protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+    Outlet,
+} from "react-router-dom";
 
 import Products from "./containers/products";
 import Details from "./components/Details";
 
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 
 import { ThemeProvider } from "@mui/material";
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
@@ -18,6 +24,7 @@ import styled from "@emotion/styled";
 import Signup from "./containers/Signup";
 import Login from "./containers/Login";
 import Auth from "./containers/Auth";
+import Checkout from "./containers/Checkout";
 // import Checkout from './containers/Checkout';
 
 let theme = createTheme();
@@ -34,6 +41,14 @@ const Wrapper = styled.div({
     },
 });
 
+const ProtectedRoute = () => {
+    const { userInfo } = useSelector((state) => state.auth);
+    if (!userInfo) {
+        return <Navigate to="/login" replace />;
+    }
+    return <Outlet />;
+};
+
 const Components = () => {
     return (
         <Provider store={store}>
@@ -45,9 +60,11 @@ const Components = () => {
                         <Route path="/" element={<HomePage />} />
                         <Route path="/signup" element={<Signup />} />
                         <Route path="/login" element={<Login />} />
-                        {/* <Route path="/details" element={<Details />} />
-              
-              <Route path="/checkout" element={<Checkout />} /> */}
+                        <Route element={<ProtectedRoute />}>
+                            <Route path="/checkout" element={<Checkout />} />
+                        </Route>
+                        {/* <Route path="/details" element={<Details />} />*/}
+                        <Route path="*" element={<Navigate to="/" replace />} />
                     </Routes>
                 </Wrapper>
             </Router>
