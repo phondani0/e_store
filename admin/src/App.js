573b73f1ea1bca9ec7514a2054cc2405df7ceb2c
phondani0 <ankitphondani52@gmail.com>
2024-04-13 23:49:50 +0530

    feat: Migrate project to use webpack config override

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,57 +1,70 @@
-import React, { Component } from 'react';
-import buildGraphQLProvider from 'ra-data-graphql-simple';
-import buildQuery from './dataProvider';
-import { Admin, Resource } from 'react-admin';
+import React, { Component } from "react";
+import buildGraphQLProvider from "ra-data-graphql-simple";
+import buildQuery from "./dataProvider";
+import { Admin, Resource } from "react-admin";
 
-import { UserList, UserCreate, UserEdit } from './Users';
-import { ProductList, ProductCreate, ProductEdit } from './Products';
-import { OrderList, OrderCreate, OrderEdit } from './Orders';
-import { createUploadLink } from 'apollo-upload-client';
+import { UserList, UserCreate, UserEdit } from "./Users";
+import { ProductList, ProductCreate, ProductEdit } from "./Products";
+import { OrderList, OrderCreate, OrderEdit } from "./Orders";
 
 // import { buildQuery } from './buildQuery';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { dataProvider: null };
-  }
-  componentDidMount() {
-    // buildGraphQLProvider({ buildQuery })
-    //         .then(dataProvider => this.setState({ dataProvider }));
+    constructor() {
+        super();
+        this.state = { dataProvider: null };
+    }
+    componentDidMount() {
+        // buildGraphQLProvider({ buildQuery })
+        //         .then(dataProvider => this.setState({ dataProvider }));
 
-    buildGraphQLProvider({
-      clientOptions: {
-        uri: process.env.REACT_APP_API_URL,
-        link: createUploadLink({
-          uri: process.env.REACT_APP_API_URL
+        console.log(
+            "process.env.REACT_APP_API_URL",
+            process.env.REACT_APP_API_URL
+        );
+        buildGraphQLProvider({
+            clientOptions: {
+                uri: process.env.REACT_APP_API_URL,
+            },
+            buildQuery: buildQuery,
         })
-      },
-      buildQuery: buildQuery,
-    })
-      .then(dataProvider => {
-        console.log(dataProvider)
-        this.setState({ dataProvider })
-      })
-      .catch(
-        error => console.log(error)
-      );
-  }
-
-  render() {
-    const { dataProvider } = this.state;
-
-    if (!dataProvider) {
-      return <div>Loading</div>;
+            .then((dataProvider) => {
+                console.log(dataProvider);
+                this.setState({ dataProvider });
+            })
+            .catch((error) => console.log(error));
     }
 
-    return (
-      <Admin dataProvider={dataProvider}>
-        <Resource name="User" list={UserList} create={UserCreate} edit={UserEdit} />
-        <Resource name="Product" list={ProductList} create={ProductCreate} edit={ProductEdit} />
-        <Resource name="Order" list={OrderList} create={OrderCreate} edit={OrderEdit} />
-      </Admin>
-    );
-  }
+    render() {
+        const { dataProvider } = this.state;
+
+        if (!dataProvider) {
+            return <div>Loading</div>;
+        }
+
+        return (
+            <Admin dataProvider={dataProvider}>
+                <Resource
+                    name="User"
+                    list={UserList}
+                    create={UserCreate}
+                    edit={UserEdit}
+                />
+                <Resource
+                    name="Product"
+                    list={ProductList}
+                    create={ProductCreate}
+                    edit={ProductEdit}
+                />
+                <Resource
+                    name="Order"
+                    list={OrderList}
+                    create={OrderCreate}
+                    edit={OrderEdit}
+                />
+            </Admin>
+        );
+    }
 }
 
 export default App;
