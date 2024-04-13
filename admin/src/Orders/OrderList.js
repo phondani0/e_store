ea2ea6abf279c4dc5b9e3dda3dc7a6702cdb141d
phondani0 <ankitphondani52@gmail.com>
2024-04-14 02:18:47 +0530

    fix: Date field in user list, product list, and order list table

--- admin/src/Orders/OrderList.js
+++ admin/src/Orders/OrderList.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import { List, Datagrid, TextField, DateField, ReferenceField } from 'react-admin';
+import React from "react";
+import { List, Datagrid, TextField, ReferenceField } from "react-admin";
+import CustomDateField from "../components/date/CustomDateField";
 
-function CustomDateField(props) {
-  const newRecord = {
-    [props.source]: parseInt(props.record[props.source])
-  }
-  return <DateField showTime {...props} record={newRecord} />
-}
-
-
-export const OrderList = props => {
-  // console.log(props)
-  return (
-    <List {...props}>
-      <Datagrid rowClick="edit">
-        <TextField source="id" />
-        <TextField source="customer_name" />
-        <TextField source="customer_email" />
-        {/* <TextField label="product" source="product.name" /> */}
-        <ReferenceField label="Product" source="product.id" reference="Product" sortBy="product.name">
-          <TextField source="name" />
-        </ReferenceField>
-        {/* <TextField label="user" source="user.first_name" /> */}
-        <ReferenceField label="User" source="user.id" reference="User" sortBy="user.first_name">
-          <TextField source="first_name" />
-        </ReferenceField>
-        <CustomDateField source="created_at" />
-        <CustomDateField source="updated_at" />
-      </Datagrid>
-    </List >
-  );
-}
+export const OrderList = (props) => {
+    // console.log(props)
+    return (
+        <List {...props}>
+            <Datagrid rowClick="edit">
+                <TextField source="id" />
+                <TextField source="customer_name" />
+                <TextField source="customer_email" />
+                {/* <TextField label="product" source="product.name" /> */}
+                <ReferenceField
+                    label="Product"
+                    source="product.id"
+                    reference="Product"
+                    sortBy="product.name"
+                >
+                    <TextField source="name" />
+                </ReferenceField>
+                {/* <TextField label="user" source="user.first_name" /> */}
+                <ReferenceField
+                    label="User"
+                    source="user.id"
+                    reference="User"
+                    sortBy="user.first_name"
+                >
+                    <TextField source="first_name" />
+                </ReferenceField>
+                <CustomDateField source="created_at" showTime />
+                <CustomDateField source="updated_at" showTime />
+            </Datagrid>
+        </List>
+    );
+};
--- admin/src/Products/ProductList.js
+++ admin/src/Products/ProductList.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import { List, Datagrid, TextField, NumberField, DateField, ImageField } from 'react-admin';
+import React from "react";
+import {
+    List,
+    Datagrid,
+    TextField,
+    NumberField,
+    ImageField,
+} from "react-admin";
+import CustomDateField from "../components/date/CustomDateField";
 
-function CustomDateField(props) {
-  const newRecord = {
-    [props.source]: parseInt(props.record[props.source])
-  }
-  return <DateField showTime {...props} record={newRecord} />
-}
-
-
-export const ProductList = props => {
-  // console.log(props)
-  return (
-    <List {...props}>
-      <Datagrid rowClick="edit">
-        <TextField source="id" />
-        <TextField source="name" />
-        <TextField source="description" />
-        <TextField source="category" />
-        <NumberField source="quantity" />
-        <NumberField source="price" />
-        <ImageField source="image" title="image" />
-        <CustomDateField source="created_at" />
-        <CustomDateField source="updated_at" />
-      </Datagrid>
-    </List >
-  );
-}
+export const ProductList = (props) => {
+    return (
+        <List {...props}>
+            <Datagrid rowClick="edit">
+                <TextField source="id" />
+                <TextField source="name" />
+                <TextField source="description" />
+                <TextField source="category" />
+                <NumberField source="quantity" />
+                <NumberField source="price" />
+                <ImageField source="image" title="image" />
+                <CustomDateField source="created_at" showTime />
+                <CustomDateField source="updated_at" showTime />
+            </Datagrid>
+        </List>
+    );
+};
