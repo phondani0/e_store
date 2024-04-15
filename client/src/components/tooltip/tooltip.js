3eb461b40782e0049d209f10a4cce6d49e000523
phondani0 <ankitphondani52@gmail.com>
2024-04-16 02:47:16 +0530

    feat: add delay in tooltip

--- client/src/components/tooltip/tooltip.js
+++ client/src/components/tooltip/tooltip.js
@@ -1,8 +1,17 @@
 import React from "react";
 import MuiTooltip from "@mui/material/Tooltip";
+import styled from "@emotion/styled";
+
+const Wrapper = styled.div({});
 
 const Tooltip = ({ content, children }) => {
-    return <MuiTooltip title={content}>{children} </MuiTooltip>;
+    // @TODO: Change background color
+    // @TODO: Fix the gap between the tooltip and mouse pointer
+    return (
+        <MuiTooltip title={content} enterDelay={1000} enterNextDelay={500}>
+            <Wrapper>{children}</Wrapper>
+        </MuiTooltip>
+    );
 };
 
 export default Tooltip;
